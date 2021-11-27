0c562e190fa8cb3d45252a86775c6147c218d339
Skylar Liang <skylarliang233@gmail.com>
2021-11-26 21:07:45 -0800

    reformatted authentication code, read endpoint from user

--- Invoker.py
+++ Invoker.py
@@ -1,60 +1,44 @@
 import json
 from optparse import OptionParser
-import os
 from requests_futures.sessions import FuturesSession
 import sys
 import time
 import threading
-# import boto3
+import requests
 
-# from Logger import ScriptLogger
 from JSONConfigHelper import CheckJSONConfig, ReadJSONConfig
 from WorkloadChecker import CheckWorkloadValidity
 from EventGenerator import GenericEventGenerator
 from GenConfigs import *
 from requests_futures.sessions import FuturesSession
-from Auth import getHeader, APIHOST, canonical_uri
-
-# logger = ScriptLogger('invoker', 'ivk.log')
-# lambda_client = boto3.client('lambda')
-RESULT = 'false'
+from Auth import IAMAuth
 
 
-def invoke(payload, instance_times):
+def invoke(auth, payload, instance_times):
     st = 0
     after_time, before_time = 0, 0
     session = FuturesSession(max_workers=15)
-    # TODO: make it dynamic from user input
-    # url = 'https://55gmzhl9w6.execute-api.us-west-1.amazonaws.com/default/test-hw'
-    url = APIHOST + canonical_uri
-    # + '/' + endpoint
-    parameters = {'blocking': False, 'result': RESULT}
+    
+    url = 'https://' + auth.host + '/' + auth.stage + '/' + auth.resource
     for t in instance_times:
         st = t - (after_time - before_time)
         if st > 0:
             time.sleep(st)
         before_time = time.time()
-        # res = lambda_client.invoke(FunctionName=function,
-        #                     InvocationType='Event', #async
-        #                     LogType='None',
-        #                     Payload=payload)
-        future = session.post(url, data=payload, headers=getHeader(payload))
-        # r = requests.post(url, data=payload, headers=getHeader(payload))
-        # print(r.text)
+        future = session.post(url, data=payload, headers=auth.getHeader(payload))
+        # r = requests.post(url, data=payload, headers=auth.getHeader(payload))
+        # print(r.status_code)
         after_time = time.time()
 
     return True
 
 def main(argv):
-    # logger.info("Workload Invoker started")
-    # print("Log file -> logs/SWI.log")
     parser = OptionParser()
     parser.add_option("-c", "--config_json", dest="config_json",
                       help="The input json config file describing the synthetic workload.", metavar="FILE")
     (options, args) = parser.parse_args()
 
     if not CheckJSONConfig(options.config_json):
-        # logger.error("You should specify a JSON config file using -c option!")
         return False
 
     workload = ReadJSONConfig(options.config_json)
@@ -64,8 +48,12 @@ def main(argv):
     [all_events, event_count] = GenericEventGenerator(workload)
     threads = []
     for (instance, instance_times) in all_events.items():
-        function = workload['instances'][instance]['application']
+        # function = workload['instances'][instance]['application']
         payload_file = workload['instances'][instance]['payload']
+        host = workload['instances'][instance]['host']
+        stage = workload['instances'][instance]['stage']
+        resource = workload['instances'][instance]['resource']
+        auth = IAMAuth(host, stage, resource)
 
         if payload_file:
             with open(payload_file, 'r') as f:
@@ -75,12 +63,10 @@ def main(argv):
             payload = None
 
         if workload['instances'][instance]['distribution'] == 'Poisson':
-            threads.append(threading.Thread(target=invoke, args=[payload, instance_times]))
+            threads.append(threading.Thread(target=invoke, args=[auth, payload, instance_times]))
 
-    # logger.info("Test started")
     for thread in threads:
         thread.start()
-    # logger.info("Test ended")
 
     return True
 
