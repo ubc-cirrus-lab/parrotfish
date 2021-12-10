58ded0da53e16856064f4f24d2831f3fa0963724
Efe Evci <efeberkeevci@gmail.com>
2021-12-10 20:57:06 +0000

    Constructed the AWSLogRetriever class

--- spot/logs/aws_log_retriever.py
+++ spot/logs/aws_log_retriever.py
@@ -1,26 +1,40 @@
 import os
 import subprocess
 import json 
+from pymongo import MongoClient
 
-def get_aws_logs(function_name = "AWSHelloWorld"):
-    if not os.path.exists("outputs"):
-        os.makedirs("outputs")
-    #get groups => for now we only have 1 group and it is hard-coded below
+class AWSLogRetriever:
+    def __init__(self):
+        super().__init__()
+    def get_aws_logs(self, function_name = "AWSHelloWorld"):
+        if not os.path.exists("logs/outputs"):
+            os.makedirs("logs/outputs")
+        #get groups => for now we only have 1 group and it is hard-coded below
 
-    #get log streams
-    streams = []
-    response = subprocess.check_output(["aws", "logs", "describe-log-streams", "--log-group-name", "/aws/lambda/"+function_name])
-    response = json.loads(response)
-    for stream in response["logStreams"]:
-        streams.append((stream["logStreamName"]))
+        #get log streams
+        streams = []
+        response = subprocess.check_output(["aws", "logs", "describe-log-streams", "--log-group-name", function_name])
+        response = json.loads(response)
+        for stream in response["logStreams"]:
+            streams.append((stream["logStreamName"]))
 
-    #get log events and save it to json files
-    for stream in streams:
-        log = subprocess.check_output(["aws", "logs", "get-log-events", "--log-group-name", "/aws/lambda/"+function_name, "--log-stream-name", stream])
-        log = json.loads(log)
-        log = json.dumps(log)
-        stream = stream.replace('/', '.')
-        with open("outputs/" +stream + ".json", "w") as file:
-            file.write(log)
+        #get log events and save it to json files
+        for stream in streams:
+            log = subprocess.check_output(["aws", "logs", "get-log-events", "--log-group-name", path, "--log-stream-name", stream])
+            log = json.loads(log)
+            log = json.dumps(log)
+            stream = stream.replace('/', '.')
+            with open("logs/outputs/" +stream + ".json", "w") as file:
+                file.write(log)
+    def save_to_db():
+        client = MongoClient('localhost', 27017)
 
-get_aws_logs()
+        db = client["mydb"]
+        collection = db['aws_logs']
+
+        outputs = os.listdir("logs/outputs")
+        for output in outputs:
+            with open("logs/outputs/"+output) as f:
+                file_data = json.load(f)
+                collection.insert_one(file_data)
+        client.close()
