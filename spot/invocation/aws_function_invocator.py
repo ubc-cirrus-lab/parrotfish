02bc19fac41bc9fd1894a04aa90cefe8d1a1edfd
Efe Evci <efeberkeevci@gmail.com>
2021-12-10 20:57:24 +0000

    Added the skeleton for aws_function-invocator

---
+++ spot/invocation/aws_function_invocator.py
@@ -0,0 +1,9 @@
+class AWSFunctionInvocator:
+    def __init__(self):
+        pass
+
+    def invoke_function(self, app_name):
+        pass
+
+    def invoke_function(self, app_name, distribution_type):
+        pass
