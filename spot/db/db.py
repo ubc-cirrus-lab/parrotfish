fc3a7ebcf42d0e2984ff056f2f7f6ec2cd8ddcc9
Efe Evci <efeberkeevci@gmail.com>
2021-12-13 05:39:52 +0000

    Implemented get_all_collection_documents

--- spot/db/db.py
+++ spot/db/db.py
@@ -4,6 +4,7 @@ import json
 from pymongo import MongoClient
 
 class DBClient:
+    #TODO: Can change creating mongoclient on constructor instead of in every function
     def __init__(self, url, port):
         self.url = url
         self.port = port
@@ -14,7 +15,6 @@ class DBClient:
         new_function_db = client[function_name]
         return
 
-
     def add_collection(self, function_name, collection_name):
         client = MongoClient(self.url, self.port)
         function_db = client[function_name]
@@ -22,15 +22,23 @@ class DBClient:
         new_collection.insert_one({})
         return
 
+    def get_all_collection_documents(self, function_name, collection_name):
+        client = MongoClient(self.url, self.port)
+        function_db = client[function_name]
+        collection = function_db[collection_name]
+        return collection.find()
+
     def add_document_to_collection(self, function_name, collection_name, document):
         client = MongoClient(self.url, self.port)
         function_db = client[function_name]
         collection = function_db[collection_name]
         collection.insert_one(document)
         return
+        
     def add_document_to_collection_if_not_exists(self, function_name, collection_name, document, criteria, value):
         client = MongoClient(self.url, self.port)
         function_db = client[function_name]
         collection = function_db[collection_name]
         if not collection.find_one({criteria : value}):
             collection.insert_one(document)
+    
