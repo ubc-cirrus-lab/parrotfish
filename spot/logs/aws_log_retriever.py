eef69fbe4aa1485de5792cf5472314b9f67db59e
Efe Evci <efeberkeevci@gmail.com>
2021-12-13 05:05:15 +0000

    Removed intermediatery json saving

--- spot/logs/aws_log_retriever.py
+++ spot/logs/aws_log_retriever.py
@@ -1,40 +1,30 @@
 import os
 import subprocess
 import json 
+from spot.db.db import DBClient
 from pymongo import MongoClient
 
 class AWSLogRetriever:
     def __init__(self):
         super().__init__()
     def get_aws_logs(self, function_name = "AWSHelloWorld"):
-        if not os.path.exists("logs/outputs"):
-            os.makedirs("logs/outputs")
-        #get groups => for now we only have 1 group and it is hard-coded below
+        client = DBClient("localhost", 27017) 
+        path = "/aws/lambda/" + function_name
 
         #get log streams
         streams = []
-        response = subprocess.check_output(["aws", "logs", "describe-log-streams", "--log-group-name", function_name])
+        response = subprocess.check_output(["aws", "logs", "describe-log-streams", "--log-group-name", path])
         response = json.loads(response)
         for stream in response["logStreams"]:
             streams.append((stream["logStreamName"]))
 
-        #get log events and save it to json files
+        #get log events and save it to DB
         for stream in streams:
             log = subprocess.check_output(["aws", "logs", "get-log-events", "--log-group-name", path, "--log-stream-name", stream])
             log = json.loads(log)
-            log = json.dumps(log)
-            stream = stream.replace('/', '.')
-            with open("logs/outputs/" +stream + ".json", "w") as file:
-                file.write(log)
-    def save_to_db():
-        client = MongoClient('localhost', 27017)
-
-        db = client["mydb"]
-        collection = db['aws_logs']
-
-        outputs = os.listdir("logs/outputs")
-        for output in outputs:
-            with open("logs/outputs/"+output) as f:
-                file_data = json.load(f)
-                collection.insert_one(file_data)
-        client.close()
+            log = log["events"][2]
+            request_id_start_pos = log["message"].find(":")+2
+            request_id_end_pos = log["message"].find("\t")
+            requestId = log["message"][request_id_start_pos:request_id_end_pos]
+            log["RequestId"] = requestId
+            client.add_document_to_collection_if_not_exists(function_name, "logs", log, "RequestId",requestId)
