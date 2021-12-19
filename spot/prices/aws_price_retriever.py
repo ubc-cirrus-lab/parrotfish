0fe1d9b080398aa1ff9958e8982fd388a3d30a94
Efe Evci <efeberkeevci@gmail.com>
2021-12-19 01:14:22 +0000

    Implemented DB saving for price retrieving

--- spot/prices/aws_price_retriever.py
+++ spot/prices/aws_price_retriever.py
@@ -1,22 +1,35 @@
 from spot.prices.price_retriever import PriceRetriever
 import os
 import json
-from pymongo import MongoClient
 import time as time
+from spot.db.db import DBClient
+
 
 class AWSPriceRetriever(PriceRetriever):
-    def __init__(self):
+    
+    def __init__(self, url, port):
         super().__init__()
+        self.DBClient = DBClient(url, port) 
+
+        
 
     def fetch_current_pricing(self, region="us-east-1") -> dict:
-        """
-        Returns the price per request and price per second as a dictionary
-
-        Keyword arguments:
-        Region -- the AWS region for pricing information (default "us-east-1")
-        """
+        current_pricing = {}
+       
         parameters = {"vendor": "aws", "service": "AWSLambda", "family": "Serverless", "region": region, "type": "AWS-Lambda-Requests", "purchaseOption": "on_demand"}
         request_price = self._current_price(parameters)
+        current_pricing["request_price"] = request_price
         parameters["type"] = "AWS-Lambda-Duration"
         duration_price = self._current_price(parameters)
-        return {"timestamp":int(time.time()),"per_request": request_price, "per_gb_second": duration_price}
+        current_pricing["duration_price"] = duration_price
+        current_pricing["timestamp"] = int(time.time()*100)
+        current_pricing["region"] = region
+
+        self.DBClient.add_document_to_collection_if_not_exists("pricing", "AWS", current_pricing, {"request_price" : request_price, "duration_price": duration_price, "region" : region})
+        
+        return current_pricing
+
+'''
+a = AWSPriceRetriever("localhost", 27017)
+a.fetch_current_pricing()
+'''
--- spot/prices/price_retriever.py
+++ spot/prices/price_retriever.py
@@ -3,12 +3,11 @@ import re
 import os
 import json
 import datetime
-
 from spot.definitions import DATA_DIR
 
 PRICES_FILE = "prices.json"
 API_URL = "https://pricing.api.infracost.io/graphql"
-API_KEY_DIR = ".config/infracost/credentials.yml"
+API_KEY_DIR = "spot/prices/credentials.yml"
 CACHE_INVALID_DAYS = 1.0
 PRICES_PATH = os.path.join(DATA_DIR, PRICES_FILE)
 
@@ -20,29 +19,13 @@ class PriceRetriever():
         self._key = ''
         self._data = self._load_data()
         self._key = self._read_api_key()
+        
 
     def get_current_prices(self, region="") -> dict:
         raise NotImplementedError
 
-    def _current_price(self, parameters) -> float:
-        """
-        Returns the cached price if the fetch date is < 1 day old. Otherwise fetches the price from the API.
-        If the fetch fails, then the outdated cached value will be returned with a warning.
-        If both fetch and cache fail, then an exception will be raised
-        """
-        try:
-            price_dict = self._get_price(parameters)
-            delta = abs(datetime.datetime.fromisoformat(price_dict["timestamp"]) - datetime.datetime.now(datetime.timezone.utc))
-            if (delta.days <= CACHE_INVALID_DAYS):
-                price = price_dict["value"]
-            else:
-                try:
-                    price = self._fetch_price(parameters)
-                except:
-                    print(f"Warning: Using a cached price that is older than the cache invalid range ({CACHE_INVALID_DAYS} days)")
-                    price = price_dict["value"]
-        except PriceNotFoundException:
-            price = self._fetch_price(parameters)
+    def _current_price(self, parameters):
+        price = self._fetch_price(parameters)
         return price
 
     #Change this to db interaction instead of file saving
@@ -103,7 +86,7 @@ class PriceRetriever():
             print(f'POST Request failed with status code {r.status_code}')
             raise PriceNotFoundException
         result_json = r.json()
-        print(result_json)
+        #print(result_json)
         try:
             price = float(result_json["data"]["products"][0]["prices"][0]["USD"])
         except:
@@ -114,7 +97,7 @@ class PriceRetriever():
 
     def _read_api_key(self) -> str:
         try:
-            with open(os.path.join(os.path.expanduser("~"), API_KEY_DIR), 'r') as file:
+            with open(API_KEY_DIR, 'r') as file:
                 for line in file.readlines():
                     match = re.search(r'api_key: (.*)', line)
                     if match:
