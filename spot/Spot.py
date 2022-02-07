3bf53ab2aa32122f409ef53ddd15eab6bd5984cc
Jacob Grossbard <jacobgrossbard@gmail.com>
2022-02-06 21:39:41 -0800

    Format with black

--- spot/Spot.py
+++ spot/Spot.py
@@ -9,18 +9,19 @@ import json
 import time as time
 import os
 
+
 class Spot:
-    def __init__(self, config_file_path = "spot/config.json"):
-        #Load configuration values from config.json
+    def __init__(self, config_file_path="spot/config.json"):
+        # Load configuration values from config.json
         self.config = None
         self.config_file_path = config_file_path
-        
+
         with open(config_file_path) as f:
             self.config = json.load(f)
-            with open(self.config["workload_path"], 'w') as json_file:
+            with open(self.config["workload_path"], "w") as json_file:
                 json.dump(self.config["workload"], json_file)
 
-        #Set environment variables
+        # Set environment variables
         aws_creds = AWSCredentialsFetch()
         os.environ["AWS_ACCESS_KEY_ID"] = aws_creds.get_access_key_id()
         os.environ["AWS_SECRET_ACCESS_KEY"] = aws_creds.get_secret_access_key()
@@ -30,38 +31,61 @@ class Spot:
         except KeyError:
             benchmark_dir = None
 
-        #Instantiate SPOT system components
-        self.price_retriever = AWSPriceRetriever(self.config["DB_URL"], self.config["DB_PORT"], self.config["region"])
-        self.log_retriever = AWSLogRetriever(self.config["function_name"], self.config["DB_URL"], self.config["DB_PORT"], self.config["last_log_timestamp"])
-        self.function_invocator = AWSFunctionInvocator(self.config["workload_path"], self.config["function_name"], self.config["mem_size"], self.config["region"])
-        self.config_retriever = AWSConfigRetriever(self.config["function_name"], self.config["DB_URL"], self.config["DB_PORT"])
-        self.ml_model = LinearRegressionModel(self.config["function_name"], self.config["vendor"], self.config["DB_URL"], self.config["DB_PORT"], self.config["last_log_timestamp"], benchmark_dir)#TODO: Parametrize ML model constructor with factory method
+        # Instantiate SPOT system components
+        self.price_retriever = AWSPriceRetriever(
+            self.config["DB_URL"], self.config["DB_PORT"], self.config["region"]
+        )
+        self.log_retriever = AWSLogRetriever(
+            self.config["function_name"],
+            self.config["DB_URL"],
+            self.config["DB_PORT"],
+            self.config["last_log_timestamp"],
+        )
+        self.function_invocator = AWSFunctionInvocator(
+            self.config["workload_path"],
+            self.config["function_name"],
+            self.config["mem_size"],
+            self.config["region"],
+        )
+        self.config_retriever = AWSConfigRetriever(
+            self.config["function_name"], self.config["DB_URL"], self.config["DB_PORT"]
+        )
+        self.ml_model = LinearRegressionModel(
+            self.config["function_name"],
+            self.config["vendor"],
+            self.config["DB_URL"],
+            self.config["DB_PORT"],
+            self.config["last_log_timestamp"],
+            benchmark_dir,
+        )  # TODO: Parametrize ML model constructor with factory method
 
     def __del__(self):
 
-        #Save the updated configurations
-        with open(self.config_file_path, 'w') as f:
+        # Save the updated configurations
+        with open(self.config_file_path, "w") as f:
             json.dump(self.config, f)
-        
+
         # Update the memory config on AWS with the newly suggested memory size
-        config_updater = ConfigUpdater(self.config["function_name"], self.config["mem_size"], self.config["region"])
+        config_updater = ConfigUpdater(
+            self.config["function_name"], self.config["mem_size"], self.config["region"]
+        )
         config_updater.set_mem_size(self.config["mem_size"])
 
     def execute(self):
         print("Invoking function:", self.config["function_name"])
-        #invoke the indicated function
+        # invoke the indicated function
         self.invoke_function()
-        
+
         print("Sleeping to allow logs to propogate")
-        #wait to allow logs to populate in aws
+        # wait to allow logs to populate in aws
         time.sleep(15)
 
         print("Retrieving new logs and save in db")
-        #collect log data
+        # collect log data
         self.collect_data()
-        
+
         print("Training ML model")
-        #train ML model accordingly
+        # train ML model accordingly
         self.train_model()
 
     def invoke_function(self):
@@ -69,16 +93,16 @@ class Spot:
         self.config_retriever.get_latest_config()
         self.price_retriever.fetch_current_pricing()
 
-        #invoke function
+        # invoke function
         self.function_invocator.invoke_all()
 
     def collect_data(self):
-        #retrieve logs
+        # retrieve logs
         self.config["last_log_timestamp"] = self.log_retriever.get_logs()
 
     def train_model(self):
         # only train the model, if new logs are introduced
         if self.ml_model.fetch_data():
-            new_configs = self.ml_model.train_model()   
+            new_configs = self.ml_model.train_model()
             for new_config in new_configs:
                 self.config[new_config] = new_configs[new_config]
--- spot/prices/aws_price_retriever.py
+++ spot/prices/aws_price_retriever.py
@@ -6,29 +6,44 @@ from spot.db.db import DBClient
 
 
 class AWSPriceRetriever(PriceRetriever):
-    
     def __init__(self, url, port, region):
         super().__init__()
-        self.DBClient = DBClient(url, port) 
+        self.DBClient = DBClient(url, port)
         self.region = region
-        
 
     def fetch_current_pricing(self) -> dict:
         current_pricing = {}
-       
-        parameters = {"vendor": "aws", "service": "AWSLambda", "family": "Serverless", "region": self.region, "type": "AWS-Lambda-Requests", "purchaseOption": "on_demand"}
+
+        parameters = {
+            "vendor": "aws",
+            "service": "AWSLambda",
+            "family": "Serverless",
+            "region": self.region,
+            "type": "AWS-Lambda-Requests",
+            "purchaseOption": "on_demand",
+        }
         request_price = self._current_price(parameters)
         current_pricing["request_price"] = request_price
         parameters["type"] = "AWS-Lambda-Duration"
         duration_price = self._current_price(parameters)
         current_pricing["duration_price"] = duration_price
-        current_pricing["timestamp"] = int(time.time()*100)
+        current_pricing["timestamp"] = int(time.time() * 100)
         current_pricing["region"] = self.region
 
-        self.DBClient.add_document_to_collection_if_not_exists("pricing", "AWS", current_pricing, {"request_price" : request_price, "duration_price": duration_price, "region" : self.region})
+        self.DBClient.add_document_to_collection_if_not_exists(
+            "pricing",
+            "AWS",
+            current_pricing,
+            {
+                "request_price": request_price,
+                "duration_price": duration_price,
+                "region": self.region,
+            },
+        )
         return current_pricing
 
-'''
+
+"""
 a = AWSPriceRetriever("localhost", 27017)
 a.fetch_current_pricing()
-'''
+"""
