1e37b71d08b467ca08d48d9bfb3a8648ef3a1df7
Yaman Malkoc <yamanmalkoc14@gmail.com>
2022-02-10 11:19:01 -0800

    black fixes

--- spot/benchmarks/aes/lambda_function.py
+++ spot/benchmarks/aes/lambda_function.py
@@ -6,17 +6,17 @@ import pyaes
 
 def generate(length):
     letters = string.ascii_lowercase + string.digits
-    return ''.join(random.choice(letters) for i in range(length))
+    return "".join(random.choice(letters) for i in range(length))
 
 
 def lambda_handler(event, context):
-    length_of_message = event['length_of_message']
-    num_of_iterations = event['num_of_iterations']
+    length_of_message = event["length_of_message"]
+    num_of_iterations = event["num_of_iterations"]
 
     message = generate(length_of_message)
 
     # 128-bit key (16 bytes)
-    KEY = b'\xa1\xf6%\x8c\x87}_\xcd\x89dHE8\xbf\xc9,'
+    KEY = b"\xa1\xf6%\x8c\x87}_\xcd\x89dHE8\xbf\xc9,"
 
     start = time()
     for loops in range(num_of_iterations):
