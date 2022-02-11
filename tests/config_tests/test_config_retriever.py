10787da64a9b8c9ce3e4dc8799577e5609115459
Yaman Malkoc <yamanmalkoc14@gmail.com>
2022-02-11 15:55:06 -0800

    packaging

---
+++ tests/config_tests/test_config_retriever.py
@@ -0,0 +1,12 @@
+
+import unittest
+
+from spot.configs.aws_config_retriever import AWSConfigRetriever
+
+class TestConfigUpdate(unittest.TestCase):
+
+    def test_getting_latest_config():
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
