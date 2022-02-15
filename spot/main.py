189b00f5356da2cfab7c9b2b34c7493a5897c9f4
Erik <eriklangille@gmail.com>
2022-02-14 21:19:21 -0800

    Screenshot app (#31)
    * Config for chrome screenshot app
    
    * CLI option to choose which benchmark to run
    
    * Updated ChromeScreenshot config to invoke properly
    
    * Fixed syntax on payload.json
    
    * Working chrome screenshot benchmark
    
    * Format with Black

--- spot/main.py
+++ spot/main.py
@@ -1,8 +1,38 @@
-from spot.benchmarks.img_proc.main import executeImageProcessing
+from getopt import getopt, error
+import sys
+from spot.benchmarks.AWSHelloWorld.main import executeAWSHelloWorld
+from spot.benchmarks.formplug.main import executeFormplug
+from spot.benchmarks.ChromeScreenshot.main import executeChromeScreenshot
+from spot.benchmarks.aes.main import executeAes
+
+# List of benchmarks and associated functions. Note: The key should be all lower case!
+benchmarks = {
+    "awshelloworld": executeAWSHelloWorld,
+    "formplug": executeFormplug,
+    "chromescreenshot": executeChromeScreenshot,
+    "aes": executeAes,
+}
 
 
 def main():
-    executeImageProcessing()
+    arguments = sys.argv[1:]
+    options = "hb:"
+    long_options = ["Help", "Benchmark"]
+
+    try:
+        args_vals, _ = getopt(arguments, options, long_options)
+
+        for arg, val in args_vals:
+            if arg in ("-h", "--Help"):
+                print("Sorry, cannot help you. 🇨🇦")
+
+            if arg in ("-b", "--Benchmark"):
+                print(val)
+                if val.lower() in benchmarks:
+                    print(f"Running benchmark: {val}")
+                    benchmarks[val.lower()]()
+    except error as err:
+        print(str(err))
 
 
 if __name__ == "__main__":
