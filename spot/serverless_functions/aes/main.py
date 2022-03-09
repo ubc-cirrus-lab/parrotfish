ec0c7136fba57623e16305b8714e8a31b981908d
Erik Langille <eriklangille@gmail.com>
2022-03-09 23:28:37 +0000

    Removed now unneccesary main.py files

--- spot/serverless_functions/aes/main.py
+++
@@ -1,7 +0,0 @@
-from spot.benchmarks.base_benchmark import BaseBenchmark
-
-
-class executeAes(BaseBenchmark):
-    def __init__(self, config="config.json"):
-        super().__init__(__file__, config=config)
-        self.run()
