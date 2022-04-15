7d09993d3a35ab5fb141e6d1eb3ce3febed648c5
Efe Evci <43101206+efeberkeevci@users.noreply.github.com>
2022-04-15 16:11:16 -0700

    Delete credentials.py

--- credentials.py
+++
@@ -1,3 +0,0 @@
-conn_str = "DefaultEndpointsProtocol=https;AccountName=storageaccountcapst8a1b;AccountKey=z4/VCf/PvapgjCFm+poml51fty8NiMzL9IdiLS8e//fdfyyxGEzgXCeZrdOapeU0G1gtsR218+CVu92LlVOJcg==;EndpointSuffix=core.windows.net"
-container_name = "insights-logs-functionapplogs"
-blob_name = "resourceId=/SUBSCRIPTIONS/B7682AB2-3937-4112-B8BA-90121A9A78A9/RESOURCEGROUPS/CAPSTONE-DEV/PROVIDERS/MICROSOFT.WEB/SITES/SPOTCAPSTONE/y=2021/m=11/d=02/h=23/m=00/PT1H.json"
