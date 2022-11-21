0a8b689e43a7b6c8f76353b8361808ec274189d2
Arshia Moghimi <arshiamoghimi98@gmail.com>
2022-11-21 13:21:07 -0800

    refactor: renamed chrome benchmark and edited markdown2html lambda code and payload

--- benchmarks/markdown2html/lambda_function.py
+++ benchmarks/markdown2html/lambda_function.py
@@ -1,12 +1,15 @@
 import markdown
+import base64
 
 
 def lambda_handler(event, context):
     try:
         text = event["markdown"]
     except:
-        return {"Error": "Possibly lacking markdown parameter in request."}
+        return {'Error': 'Possibly lacking markdown parameter in request.'}
 
-    html = markdown.markdown(text)
+    decoded_text = base64.b64decode(text.encode()).decode()
+
+    html = markdown.markdown(decoded_text)
 
     return {"html_response": html}
