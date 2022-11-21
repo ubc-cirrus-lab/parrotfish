08ed0935938c0753343820b171413e1633eb1ccc
hattori <joe2ninja21@gmail.com>
2022-11-21 14:23:53 -0800

    add benchmarks (#100)
    * add benchmarks
    
    * Format with Black
    
    * refactor: renamed chrome benchmark and edited markdown2html lambda code and payload
    
    * Format with Black
    
    * add source codes
    
    Co-authored-by: Github Actions Bot <>
    Co-authored-by: Arshia Moghimi <arshiamoghimi98@gmail.com>

---
+++ benchmarks/formplug/README.md
@@ -0,0 +1,2 @@
+This benchmark is from [https://github.com/danielireson/formplug](https://github.com/danielireson/formplug).
+Follow the [instructions](https://github.com/danielireson/formplug#setup) to deploy it to AWS.
---
+++ benchmarks/markdown2html/lambda_function.py
@@ -0,0 +1,15 @@
+import markdown
+import base64
+
+
+def lambda_handler(event, context):
+    try:
+        text = event["markdown"]
+    except:
+        return {"Error": "Possibly lacking markdown parameter in request."}
+
+    decoded_text = base64.b64decode(text.encode()).decode()
+
+    html = markdown.markdown(decoded_text)
+
+    return {"html_response": html}
