2a50d79e4abd8d56fd0454c67bd534996acf4c4b
MehdiBC <mehdi.b.chikha@gmail.com>
2023-08-03 02:23:05 +0100

    chore: update ci and added cd

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
-import sys
+import os
+
 from setuptools import setup, find_packages
 
-if sys.version_info.major != 3:
-    print("This code is only compatable with Python 3")
+version = os.environ['PACKAGE_VERSION']
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as r:
+    requirements = r.read()
+
+with open("requirements-dev.txt", "r") as rdev:
+    requirements_dev = rdev.read()
+
+#TODO: Update licence attribute
 setup(
     name="parrotfish",
-    packages=[package for package in find_packages() if package.startswith("src")],
-    package_data={
-        "src": ["py.typed"],
-    },
-    install_requires=[],
-    extras_require={},
-    entry_points={"console_scripts": ["parrotfish=src.main:main"]},
+    version=version,
     description="Parametric Regression for Optimizing Serverless Functions",
-    version="0.0.1",
+    packages=[package for package in find_packages() if package.startswith("src")],
+    entry_points={"console_scripts": ["parrotfish=src.main:main"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubc-cirrus-lab/parrotfish",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements.split("\n"),
+    extras_require={
+        "dev": requirements_dev.split("\n"),
+    },
+    python_requires=">=3.8",
 )
